8e723ec8459795dd6ca844877bdd9c6571439551
Kaculoss <benjerosani99@gmail.com>
2022-07-07 05:09:11 +0000

    finished my about section

--- 00-my_website/src/Pages/Home.js
+++ 00-my_website/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AboutSection from "../Components/AboutSection";
 import HeroSection from "../Components/HeroSection";
 import { ScrollToBottom, ScrollToTop } from "../Components/ScrollButton";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
       <ScrollToTop />
       <ScrollToBottom />
     </div>
