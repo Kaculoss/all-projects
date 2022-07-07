87b5b60420fdca3482b904f78cbc31e8e87da8bd
Kaculoss <benjerosani99@gmail.com>
2022-07-07 14:25:59 +0000

    finished my services section

--- 00-my_website/src/Pages/Home.js
+++ 00-my_website/src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import AboutSection from "../Components/AboutSection";
 import HeroSection from "../Components/HeroSection";
 import { ScrollToBottom, ScrollToTop } from "../Components/ScrollButton";
+import ServiceSection from "../Components/ServiceSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <ServiceSection />
       <ScrollToTop />
       <ScrollToBottom />
     </div>
